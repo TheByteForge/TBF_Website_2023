c60637c2919dfdd81a32bc5d409b60d1b3bebced
Zach Watkins <zackwatkins@gmail.com>
2023-01-23 12:14:28 -0800

    fix: corrected color in Main

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -21,7 +21,7 @@ const Main = (props: IMainProps) => (
           <div className="text-xl">{AppConfig.description}</div>
         </div>
         <div>
-          <ul className="flex flex-wrap text-xl">
+          <ul className="flex flex-wrap bg-gray-200 text-xl">
             <li className="mr-6">
               <Link
                 href="/"
@@ -69,14 +69,7 @@ const Main = (props: IMainProps) => (
       <div className="content py-5 text-xl">{props.children}</div>
 
       <div className="border-t border-gray-300 py-8 text-center text-sm">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{' '}
-        <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>.
-        {/*
-         * PLEASE READ THIS SECTION
-         * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-         * The link doesn't need to appear on every pages, one link on one page is enough.
-         * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-         */}
+        © Copyright {new Date().getFullYear()} {AppConfig.title}.
       </div>
     </div>
   </div>
